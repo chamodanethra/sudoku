c0c3416f1e20640afc7100bf3b2b123758adc841
Chamoda Nethra <chamodananayakkara@gmail.com>
2019-12-26 22:02:03 +0530

    Updates

---
+++ precache-manifest.53f3f9113cfcc091404600485288fe26.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74b4775898a7812c1c0f",
+    "url": "/sudoku/3ed97a1b3b8a2382fa5b.module.wasm"
+  },
+  {
+    "revision": "702c3880d4b6f20bd432cc3b137f5411",
+    "url": "/sudoku/index.html"
+  },
+  {
+    "revision": "200e2ffdb4ce9bca5b54",
+    "url": "/sudoku/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "6310fc4714276fb725a2",
+    "url": "/sudoku/static/css/main.0484070f.chunk.css"
+  },
+  {
+    "revision": "200e2ffdb4ce9bca5b54",
+    "url": "/sudoku/static/js/2.139ce92d.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/sudoku/static/js/2.139ce92d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "74b4775898a7812c1c0f",
+    "url": "/sudoku/static/js/3.704a1155.chunk.js"
+  },
+  {
+    "revision": "6310fc4714276fb725a2",
+    "url": "/sudoku/static/js/main.fafa1e7e.chunk.js"
+  },
+  {
+    "revision": "c00eb399d3bae985633b",
+    "url": "/sudoku/static/js/runtime-main.a708b095.js"
+  },
+  {
+    "revision": "640ae25e6cd064ecf81e6e83df85e48d",
+    "url": "/sudoku/static/media/p096_sudoku.640ae25e.txt"
+  }
+]);
